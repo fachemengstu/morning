77db33f0af04ff5d8817ff14cd0886b0f199b591
fachemengstu <74604275+fachemengstu@users.noreply.github.com>
2023-06-14 03:04:53 +0200

    streamlitsharing.py

--- streamlitsharing.py
+++ streamlitsharing.py
@@ -1,95 +1,38 @@
-import streamlit as st
+from collections import namedtuple
+import altair as alt
+import math
 import pandas as pd
-import numpy as np
-from pandas.io.sas import sas7bdat
-import sas7bdat
-import plotly.express as px
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-st.title("Summary analysis on the ECL dataset  ")
-st.sidebar.title("Summary analysis on the ECL dataset ")
+import streamlit as st
 
-st.markdown("This application is streamlit dashboard to analysze the ECl Dataset for Banca Ifis  ")
-st.sidebar.markdown("This application is streamlit dashboard to analysze the ECl Dataset for banca ifis")
-# Read data from the 'Ead_dis_rating_March' sheet in an Excel file and save as CSV
-df1= ('Y:\RISK_SM\Stage_Futsum\Total_Summary_Dec&Mar.xlsx')
+"""
+# Welcome to Streamlit!
 
-@st._cache_data(persist=True)
-def load_data():
-    data = pd.read_excel(df1, sheet_name='SummaryTable', engine='openpyxl')
-    data=data.to_csv('C:/Users/fmuche/Downloads/RatingModels79.csv', index=False)
-    data=pd.read_csv('C:/Users/fmuche/Downloads/RatingModels79.csv')
-    return data
-data = load_data()
-#data2 = load_data()
-#st.write(data2)
-if st.checkbox("** click Here To see the Ecl dataset for December 2022 **" , False):
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-    #Rating_models = data.loc[(data['DESC_SOTTOMODELLO'].isin(['Small Business', 'Corporate ML', 'Corporate Small']))]
-    rating_model = data.groupby(by="DESC_SOTTOMODELLO").sum(numeric_only=True)[["EAD_CURRENT"]]
-    business_area = data.groupby(by="business_area").sum(numeric_only=True)[["EAD_CURRENT"]]
-    #Rating_modelsAgg=Rating_models.groupby(by="DESC_SOTTOMODELLO").sum(numeric_only=True)[["EAD_CURRENT"]]
-    #Rating_modelsAgg_busness = Rating_models.groupby(by="business_area").sum(numeric_only=True)[["EAD_CURRENT"]]
-    st.subheader('ECL dataset')
-    st.write(data)
-    st.write(data.shape)
-    st.subheader('Dataset for the rating models')
-    #st.write(Rating_models)
-    #st.write(Rating_models.shape)
-    st.subheader('The columns we have in the dataset')
-    st.write(data.columns)
-    st.subheader('The sum of Ead_current by Models ')
-    st.write(rating_model)
-    st.subheader('The sum Ead_current by Business Area')
-    st.write(business_area)
-    st.subheader('The aggregate Ead_current for Rating Models ')
-    #st.write(Rating_modelsAgg)
-    st.subheader('The aggregate Ead_current for Rating Models by Business Area')
-    #st.write(Rating_modelsAgg_busness)
-    #st.write(data2)
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
 
-#st.sidebar.subheader("Show Me EAD value for rating Rating Models")
-#sum_ead_value=st.sidebar.radio('DESC_SOTTOMODELLO',('Corporate ML','Corporate Small','Small Business'))
-#st.sidebar.markdown(data.query(data['total_ead_current_Dec']=='sum_ead_value').sum())
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
+    Point = namedtuple('Point', 'x y')
+    data = []
 
-st.sidebar.markdown('EAD_value by business area and rating models')
-select=st.sidebar.selectbox('Visualization type', ['Histogram','pie chart'],key='1')
+    points_per_turn = total_points / num_turns
 
-#rating_model=pd.DataFrame({'DESC_SOTTOMODELLO':data.index,'tot_ead_current_mar':data.values})
-st.markdown('### total ead by rating model for Mach')
-if not st.sidebar.checkbox("Hide",True):
-    if select=='Histogram':
-       fig=px.bar(data,x='DESC_SOTTOMODELLO',y='EAD_CURRENT',color='DESC_SOTTOMODELLO',height=500,width=800)
-       #percent=st.sidebar.slider('Percentage',0,100,100)
-       #for i, val in enumerate(data['EAD_CURRENT'].values):
-           #plt.text(i,val,"{:.2f}%".format(val/data['DESC_SOTTOMODELLO'].sum()*100),fontsize=10)
-           #fig.update_traces(marker_color=percent)
-       st.plotly_chart(fig)
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-
-    else:
-        fig=px.pie(data,values='EAD_CURRENT',names='DESC_SOTTOMODELLO')
-        st.plotly_chart(fig)
-
-st.sidebar.markdown('EAD_value by business area ')
-select=st.sidebar.selectbox('Visualization type', ['Histogram','pie chart'],key='2')
-if not st.sidebar.checkbox("Hide",True ,key='3'):
-    st.markdown('### total ead by business  area for Mach')
-    if select == "Histogram":
-        fig1=px.bar(data,x='business_area',y='EAD_CURRENT',color='business_area',height=600)
-        st.plotly_chart(fig1)
-    else:
-        fig1=px.pie(data,values='EAD_CURRENT',names='business_area')
-        st.plotly_chart(fig1)
-
-st.subheader("Ead distribution for different months")
-choice= st.sidebar.multiselect('Pick different months',('Feburary','March','December'),key='4')
-if len(choice)>0:
-    choice_data=data[data.REPORTING_DT.isin(choice)]
-    fig_choice=px.histogram(choice_data , x='REPORTING_DT', y='EAD_CURRENT', histfunc='sum', color='DESC_SOTTOMODELLO',
-                            facet_col='DESC_SOTTOMODELLO',labels={'Rating_model':'EAD_CURRENT'}, height=600, width=800)
-    st.plotly_chart(fig_choice)
-
-
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
